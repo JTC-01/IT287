abb770e728709fd55fa5480672b4625904f96fcc
jtc <jtc.01@outlook.com>
2025-11-25 22:57:07 -0800

    Added prints for specific dice rolls

--- dice.py
+++ dice.py
@@ -5,15 +5,37 @@ from config import p, e
 def roll_dice(sides=6, count=1):
     rolls = list()
     total = 0
+
+    p("=== Dice Roll Started ===")
+
     p(f"Rolling {count} dice with {sides} sides.")
 
     for i in range(count):
         roll = random.randint(1, sides)
         total += roll
         rolls.append(roll)
+
         p(f"Rolled {roll} - Total: {total}")
     
     p(f"Total of rolls: {total}")
+    if total == 1:
+        p("You rolled a lonely 1!")
+    elif total == 2:
+        p("Snake Eyes! Double 1s rolled!")
+    elif total == 3:
+        p("Three's company! You rolled 3!")
+    elif total == 6:
+        p("Half a dozen! You rolled 6!")
+    elif total == 7:
+        p("Lucky! You rolled 7!")
+    elif total == 13:
+        p("Unlucky! You rolled 13!")
+    elif total == 21:
+        p("Blackjack! You rolled 21!")
+
+    p("Returning results to game.")
+
+    p("=== Dice Roll finished ===")
 
     return rolls, total
 
